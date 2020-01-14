9d843dab2f96c41c54989df9d7dbf8702485040a
Muhammad Yusuf <muhammadyusuf12007@example.com>
2020-01-14 13:44:22 +0700

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -2,3 +2,4 @@ from django.shortcuts import render
 
 def post_list(request):
     return render(request, 'blog/post.html', {})
+    Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
